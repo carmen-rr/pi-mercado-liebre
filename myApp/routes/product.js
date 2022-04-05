7f95093ec8e01195deaa5d0bd29ff43c088623ec
carmen-rr <crodriguezreynoso@udesa.edu.ar>
2022-04-05 13:23:35 -0300

    agregando module exports y guardando

--- myApp/routes/product.js
+++ myApp/routes/product.js
@@ -2,5 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const productController = require('../controllers/productController');
+
+
 router.get('/', productController.show) // tenemos que ver que modo usamos 
 
+module.exports = router; 
--- myApp/routes/profile.js
+++ myApp/routes/profile.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const profileController = require('../controllers/profileController');
+
+
 router.get('/', profileController.show) // tenemos que ver que modo usamos 
+
+module.exports = router; 
