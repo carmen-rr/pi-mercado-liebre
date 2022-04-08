541b07e488756ef475f14db7e4712233f421d925
tsanzbelloni <tsanzbelloni@udesa.edu.ar>
2022-04-08 13:39:49 -0300

    botones de formularios llevan a destino logico

--- myApp/controllers/userController.js
+++ myApp/controllers/userController.js
@@ -4,9 +4,9 @@ const userController = {
 
     //RUTA DE PROFILE 
     profile: function(req,res){
-        let nombreUsuario = db.usuario
-        return res.render('profile', { 
-          dataUsuario: nombreUsuario 
+        let nombreUsuario = db.usuario //todo lo que quiero que aparezca de la db en la vista
+        return res.render('profile', {
+          dataUsuario: nombreUsuario //identificar que esta toda la info del usuario Nombre usuario tiene el contenido
         })
       }, 
 
