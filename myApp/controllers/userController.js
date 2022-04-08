f4aa04718ace5fad98adf4d32ec430c942c7adc7
crodriguezreynoso@udesa.edu.ar <root@Carmens-MacBook-Pro-2.local>
2022-04-08 15:07:06 -0300

    cambiando db

--- myApp/controllers/userController.js
+++ myApp/controllers/userController.js
@@ -5,16 +5,19 @@ const userController = {
     //RUTA DE PROFILE 
     profile: function(req,res){
         let nombreUsuario = db.usuario //todo lo que quiero que aparezca de la db en la vista
+        let nombreProducto = db.producto;
         return res.render('profile', {
-          dataUsuario: nombreUsuario //identificar que esta toda la info del usuario Nombre usuario tiene el contenido
+          dataUsuario: nombreUsuario, //identificar que esta toda la info del usuario Nombre usuario tiene el contenido
+          dataProducto: nombreProducto,
         })
       }, 
 
     //RUTA DE PROFILE-EDIT  
     profileEdit:  function(req,res){
-        let nombreUsuario = db.usuario
+        let nombreUsuario = db.usuario;
         return res.render('profile-edit', {
-        dataUsuario: nombreUsuario
+        dataUsuario: nombreUsuario, 
+        
         })
       }, 
 
